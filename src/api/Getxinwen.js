29d88f6202173c267f3b5e1f9516e73365efa967
holoto <laibin45@gmail.com>
2019-04-01 23:30:12 +0800

    fix

--- src/api/Getxinwen.js
+++ src/api/Getxinwen.js
@@ -1,12 +1,15 @@
-
+// https://wx.jdcloud.com/user/baseInfo
+// api data
+const myjdapikey = "4167297cce18bcf24ecae7b60c50f819"
+// set you jd api key
 const apiurl={
   baseurl: 'https://way.jd.com/',
   channel: 'jisuapi/channel',
   selectchannel: 'jisuapi/get?channel=',
   selectmaxnum: '&num=',
   selectbasestart: '&start=',
-  withapikey: '&appkey=4167297cce18bcf24ecae7b60c50f819',
-  apikeyurl: '?appkey=4167297cce18bcf24ecae7b60c50f819',
+  withapikey: '&appkey='+myjdapikey,
+  apikeyurl: '?appkey='+myjdapikey,
   searchkeyword: 'jisuapi/newSearch?keyword='
 }
 
